19e851934638a605a3b1ca73eaf473762d052b04
makeba <amka32904@example.com>
2026-01-02 16:29:59 +0800

    home

--- pages/home.js
+++ pages/home.js
@@ -2,10 +2,8 @@ export function HomePage() {
   return `
     <search-bar></search-bar>
     <food-info-app 
-    food-url="./data-home-page-food.json"
-    restaurant-url="./data-home-page-restaurant.json">
+    food-url="http://localhost:3000/api/foods"
+    restaurant-url="http://localhost:3000/api/restaurants">
     </food-info-app>
   `;
 }
-    // food-url="http://localhost:3000/api/foods"
-    // restaurant-url="http://localhost:3000/api/restaurants">
